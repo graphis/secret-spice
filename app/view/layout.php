77ea11bbdd7c2e82403f211b334febbcea162a81
Zsolt Sándor <ezsolt@gmail.com>
2015-09-03 12:19:18 +0200

    layout variables

--- app/view/layout.php
+++ app/view/layout.php
@@ -1,8 +1,32 @@
 ___LAYOUT___
 <pre>
-<?php print_r( $data ); ?>
+<?php 
+if (isset($data)) {
+  print_r( $data );
+} else {
+  // echo "not set";
+}
+?>
+
+
+
+
+<?php
+if (isset($crypto_data)) {
+  print_r( $crypto_data );
+} else {
+  // echo "not set";
+}
+?>
+
+
 </pre>
+
+
+
 ___end_LAYOUT___
+
+
 <!-- profile -->
 <?php
 // profile
@@ -11,3 +35,6 @@ $time = $time_end - APP_START_TIME;
 echo "<br/><br/><br/>Page rendered in $time seconds<br/>";
 echo 'memory: ' . number_format((memory_get_peak_usage() - APP_MEMORY_USAGE) / 1024, 4).'kb';
 ?>
+
+
+
